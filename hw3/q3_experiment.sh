e41749c34f340d9d84868015c061f474e49d729e
Steven Chen <sccchenster@gmail.com>
2018-10-09 22:33:00 -0700

    Add param experiment file

---
+++ hw3/q3_experiment.sh
@@ -0,0 +1,6 @@
+echo "Running LR=0.5"
+python run_dqn_atari_experiment.py 0.5
+echo "Running LR=4.0"
+python run_dqn_atari_experiment.py 4
+echo "Running LR=8.0"
+python run_dqn_atari_experiment.py 8
